78d6edf0cb339587f22c9440106aa1e527497218
Kerollos Magdy <kerolloz@yahoo.com>
2020-02-17 04:28:59 +0200

    add validation to contact us controller

--- app/models/contact_us.rb
+++ app/models/contact_us.rb
@@ -1,8 +1,13 @@
 class ContactUs
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :message
   field :email
   field :name
-  validates_presence_of :message, :email, :name
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  field :from
+
+  validates_presence_of :message, :email, :name, :from
+  validates :email, email: true
+  validates :from, inclusion: { in: %w[WEB MOB],
+                                message: '%{value} must be either WEB or MOB' }
 end
