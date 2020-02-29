c20ad289525350312e5e0695a6608befbae468e1
Kerollos Magdy <kerolloz@yahoo.com>
2020-02-29 05:09:58 +0200

    use service_sid from env var instead of hard coded string

--- lib/twilio_verification.rb
+++ lib/twilio_verification.rb
@@ -4,16 +4,13 @@ require 'twilio-ruby'
 
 # Verify phone_number using Twilio Verify API
 class TwilioVerification
-
   account_sid = ENV['TWILIO_ACCOUNT_SID']
   auth_token = ENV['TWILIO_AUTH_TOKEN']
-  pp account_sid, auth_token
+  service_sid = ENV['TWILIO_SERVICE_SID']
   @client = Twilio::REST::Client
             .new(account_sid, auth_token)
-            .verify.services('VA0d0959f30e22dcee0e6ad5c84fea11ff')
-
+            .verify.services(service_sid)
   class << self
-
     def send_code_to(phone_number)
       @client.verifications.create(to: phone_number, channel: 'sms').status
     end
@@ -23,6 +20,5 @@ class TwilioVerification
              .create(to: phone_number, code: code)
              .status == 'approved'
     end
-
   end
 end
