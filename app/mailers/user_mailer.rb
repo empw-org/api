59f9b2781e70f445bdc8e7adf021fa510a0eb54a
Kerollos Magdy <kerolloz@yahoo.com>
2020-06-10 22:44:59 +0200

    remove logging

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,6 @@ class UserMailer < ApplicationMailer
 
   def contact_us_email(contact_us_id)
     @contact_us = ContactUs.find(contact_us_id)
-    puts 'Sending email to '
-    p email_with_name(@contact_us)
     mail(to: email_with_name(@contact_us), subject: suffix_subject('We got it'))
   end
 end
