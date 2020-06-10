3f0816163740a7a54d3f58cf253774a2eb9cdf93
Kerollos Magdy <kerolloz@yahoo.com>
2020-06-10 22:46:18 +0200

    rename @current_user to @authenticated_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
   before_action :authenticate_request
-  attr_reader :current_user
+  attr_reader :authenticated_user
 
   private
 
   def authenticate_request
-    @current_user = AuthenticateApiRequest.call(request.headers).result
-    return if @current_user
+    @authenticated_user = AuthenticateApiRequest.call(request.headers).result
+    return if @authenticated_user
 
     render json: { error: 'Not Authorized' }, status: :unauthorized
   end
+
 end
