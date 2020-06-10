faa0f10dd1bc85621f1c53b723b14799c329ec71
Kerollos Magdy <kerolloz@yahoo.com>
2020-06-10 22:10:51 +0200

    use Model.id instead of _id

--- lib/token_maker.rb
+++ lib/token_maker.rb
@@ -2,10 +2,9 @@ class TokenMaker
   class << self
     def for(doc)
       type = doc.class.name.upcase
-      JsonWebToken.encode({ _id: doc._id.to_s, type: type })
+      JsonWebToken.encode({ _id: doc.id.to_s, type: type })
     end
   end
-
 end
 
 
