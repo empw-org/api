36bc193ce5636f045960587c04eaef2208de1754
Kerollos Magdy <kerolloz@yahoo.com>
2020-06-12 18:22:28 +0200

    handle wrong verification code

--- app/auth/twilio_verification.rb
+++ app/auth/twilio_verification.rb
@@ -7,9 +7,9 @@ class TwilioVerification
   account_sid = ENV['TWILIO_ACCOUNT_SID']
   auth_token = ENV['TWILIO_AUTH_TOKEN']
   service_sid = ENV['TWILIO_SERVICE_SID']
-  @client = Twilio::REST::Client
-            .new(account_sid, auth_token)
-            .verify.services(service_sid)
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+                                .verify.services(service_sid)
+
   class << self
     def send_code_to(phone_number)
       @client.verifications.create(to: phone_number, channel: 'sms').status
@@ -19,6 +19,8 @@ class TwilioVerification
       @client.verification_checks
              .create(to: phone_number, code: code)
              .status == 'approved'
+    rescue Twilio::REST::RestError
+      nil
     end
   end
 end
