a3a99d156174ff1e1f29eb8a1be1a1ea030a8fb6
Kerollos Magdy <kerolloz@yahoo.com>
2020-07-17 13:46:00 +0200

    remove logging for water order job

--- app/jobs/water_order_job.rb
+++ app/jobs/water_order_job.rb
@@ -3,28 +3,22 @@ class WaterOrderJob < ApplicationJob
   attr_reader :water_order
 
   before_perform do |job|
-    puts 'Setting water order for id'
     id = job.arguments.first
     @water_order = WaterOrder.find(id)
   end
 
   class Pending < WaterOrderJob
-    def perform(id)
-      puts 'A Pending Water Order'
+    def perform
       nearest_company = Company
                         .near_sphere(location: @water_order.location)
                         .limit(1).to_a.first
       @water_order.update(company: nearest_company,
                           state: WaterOrder::ASSIGNED_TO_COMPANY)
-      puts 'assigned to company'
-      pp @water_order.company
     end
   end
 
   class ReadyForShipping < WaterOrderJob
     def perform(*args)
-      puts 'A ReadyForShipping Water Order'
-      pp args
     end
   end
 end
