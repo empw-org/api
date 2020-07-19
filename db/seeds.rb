1fc93898b1a82f521234085647f4779ac410b642
Kerollos Magdy <kerolloz@yahoo.com>
2020-07-19 14:16:31 +0200

    remove temporary companies from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
 # frozen_string_literal: true
-
-[
-  { name: 'EMPW-Suez-up', location: { lat: 29.981881, long: 32.545291 } },
-  { name: 'EMPW-Suez-down', location: { lat: 29.981941, long: 32.549868 } }
-].each { |company| Company.create(company) }
-
 Admin.create({ email: 'empw.org@gmail.com', password: ENV['ADMIN_PASSWORD'] })
