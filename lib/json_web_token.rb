7835e7ead29bc5b7ef628e453e124d7798845731
Kerollos Magdy <kerolloz@yahoo.com>
2020-10-08 05:02:28 +0200

    fix secret_key_base nil on Heroku

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,12 @@
 class JsonWebToken
   class << self
     def encode(payload)
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.dig(:secret_key_base))
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token,
+                        Rails.application.credentials.dig(:secret_key_base))[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
